915e3f97fe4e452a1816069aeddbec91f5fc2a10
dondi3go <fake@fake.com>
2018-06-10 14:26:37 +0200

    Change folder names in Tests

---
+++ Tests/UnitTests/ProceduralMeshTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Plasticine;
+
+using NUnit.Framework;
+
+namespace Plasticine {
+
+    [TestFixture]
+    public class ProceduralMeshTest {
+
+        public ProceduralMeshTest() {
+        }
+
+        [Test]
+        public void Create()
+        {
+            //ProceduralMesh pmesh = new ProceduralMesh ();
+            int i=2;
+            Assert.AreEqual (i, 1);
+        }
+
+    }
+}
